c612f78b5d46f4edd1894798875e453e35e8fc47
Mohamed Nehad <mohammednehad550@gmail.com>
2020-12-17 17:10:17 +0200

    fix: Fixed frontend url with a catch all path

--- todo/urls.py
+++ todo/urls.py
@@ -1,6 +1,8 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
+    # Catch all: serves React frontend
     path('', views.frontend),
+    re_path(r'^.*/$', views.frontend),
 ]
