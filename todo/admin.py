660765a148653a25a01a362d58b1948cb5496b0a
Mohamed Nehad <mohammednehad550@gmail.com>
2021-01-02 14:18:15 +0200

    feat: Add models to backend

--- todo/admin.py
+++ todo/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
+from todo.models import Todo, Step, Tag
+
+
+class TodoAdmin(admin.ModelAdmin):
+    list_display = ("title", "checked",
+                    "tag_label", "date", "steps_count", "id")
+
+    def tag_label(self, obj):
+        return obj.tag.label
+
+    def steps_count(self, obj):
+        return obj.steps.count()
+
+
+class TagAdmin(admin.ModelAdmin):
+    list_display = ("label", "color", "id")
+
+
+class StepAdmin(admin.ModelAdmin):
+    list_display = ("title", "todo_title", "checked", "id")
+
+    def todo_title(self, obj):
+        return obj.todo.title
+
 
 # Register your models here.
+admin.site.register(Todo, TodoAdmin)
+admin.site.register(Step, StepAdmin)
+admin.site.register(Tag, TagAdmin)
--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,38 @@
+import uuid
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+def get_default_tag():
+    return Tag.objects.get(label='None')
+
+
+class Tag(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, blank=True, null=True, related_name="tags")
+    color = models.CharField(max_length=7)
+    label = models.CharField(max_length=64)
+
+
+class Todo(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='todos')
+    title = models.CharField(max_length=150, unique=True)
+    checked = models.BooleanField(default=False)
+    tag = models.ForeignKey(Tag, on_delete=models.SET_DEFAULT,
+                            default=get_default_tag)
+    dueDate = models.DateTimeField(blank=True, null=True, )
+    date = models.DateTimeField()
+
+
+class Step(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    title = models.CharField(max_length=150)
+    dueDate = models.DateTimeField(blank=True, null=True)
+    checked = models.BooleanField(default=False)
+    todo = models.ForeignKey(Todo, related_name='steps',
+                             on_delete=models.CASCADE)
